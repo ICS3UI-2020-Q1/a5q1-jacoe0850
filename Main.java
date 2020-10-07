41d9e2705c3e45a78aaacd68fc0ca6fe4b2036c2
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-07 03:57:39 +0000

    Finished multiplication tables

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * makes 1-12 multiplication table of integer
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create scanner for input
+   Scanner input = new Scanner(System.in);
+   // Ask for integer
+   System.out.println("Please enter an integer to create a multiplication table for");
+   // create variables
+   int num = input.nextInt();
+   int product;
+   // for loop calculations
+   for(int count = 1; count <= 12; count++){
+   product = num*count;
+   // all products
+   System.out.println(count + " " + "x" + " " + num + " " + "=" + " " + product);
+  }
   }
 }
